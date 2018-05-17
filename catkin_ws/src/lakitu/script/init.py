dca3ba90b39123c35d7cb529b7480308b5d56879
wesfletch <wesfletch@gmail.com>
2018-05-17 19:52:35 -0400

    trying stuff in init

--- catkin_ws/src/lakitu/script/init.py
+++ catkin_ws/src/lakitu/script/init.py
@@ -3,14 +3,22 @@
 import rospy, mavros
 import math
 from mavros_msgs.srv import CommandBool, SetMode
-from mavros_msgs.msg import State
+from mavros_msgs.msg import State, RCIn
 from geometry_msgs.msg import PoseStamped, TwistStamped
 from custom_msgs.msg import StateMachine
 
+rcNum = None
+
+def callback(data):
+
+	global rcNum
+	rcNUm = data.channels[6]
+	print(rcNum)
 
 if __name__=='__main__':
 
 	rospy.init_node("init_node", anonymous=True)
+	rospy.Subscriber("/mavros/rc/in", RCIn, callback)
 	init_state_pub = rospy.Publisher('/state_machine/state', StateMachine, queue_size=100, latch=True)
 
 	state = StateMachine()
