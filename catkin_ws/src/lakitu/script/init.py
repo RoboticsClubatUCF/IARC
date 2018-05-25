c2a0ee9a8f067bd27df1e660d310bab5fcf56276
Matthew Kurtz <matthewryankurtz@gmail.com>
2018-05-24 20:37:12 -0400

    adding in switch controls (bugs)

--- catkin_ws/src/lakitu/script/init.py
+++ catkin_ws/src/lakitu/script/init.py
@@ -1,17 +1,23 @@
 #!/usr/bin/env python
 
 import rospy, mavros
-import math
+import math, numpy
 from mavros_msgs.srv import CommandBool, SetMode
 from mavros_msgs.msg import State, RCIn
 from geometry_msgs.msg import PoseStamped, TwistStamped
 from custom_msgs.msg import StateMachine
 
+rcNum = None
+
+def callback(data):
+
+	global rcNum
+	rcNum = data.channels[6]
 
 if __name__=='__main__':
 
 	rospy.init_node("init_node", anonymous=True)
-	rospy.Subscriber("/mavros/rc/in", RCIn, queue_size=100)
+	rospy.Subscriber("/mavros/rc/in", RCIn, callback)
 	init_state_pub = rospy.Publisher('/state_machine/state', StateMachine, queue_size=100, latch=True)
 
 	state = StateMachine()
@@ -22,11 +28,9 @@ if __name__=='__main__':
 	state.land = False
 	state.emergency = False
 
-
 	rate = rospy.Rate(60)	
 
 	flag = True
-
 	while not rospy.is_shutdown():
 		
 		if rcNum == 2113 and flag:
